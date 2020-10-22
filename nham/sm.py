6578585a270d582aefdbccc7f96879079c542b84
Giovanni Venâncio <gvs11ufpr@gmail.com>
2020-10-22 09:36:26 -0300

    Finished restore mechanism.
    Signed-off-by: Giovanni Venâncio <gvs11ufpr@gmail.com>

--- nham/sm.py
+++ nham/sm.py
@@ -8,6 +8,7 @@ import_vnf_state
 export_vnf_state
 """
 
+import ctypes
 import os
 import re
 import requests
@@ -23,20 +24,41 @@ from multiprocessing import Process, Queue
 from flask import request, jsonify
 from utils import *
 
+## ptrace(2) interface
+c_ptrace = ctypes.CDLL("libc.so.6").ptrace
+c_pid_t = ctypes.c_int32
+c_ptrace.argtypes = [ctypes.c_int, c_pid_t, ctypes.c_void_p, ctypes.c_void_p]
+
 app = Eve()
 
 VIM_URL = 'http://0.0.0.0:9000/vim/'
 VNF_URL = 'http://0.0.0.0:9001/vnf/'
 SFC_URL = 'http://0.0.0.0:9002/sfc/'
 
-_db_path = '/tmp/'
+memory_permissions = 'rw'
+sync_workers = {}
+
+def ptrace(attach, pid):
+    """ATTACH or DETACH a process.
+    Used to stop/resume execution when a checkpoint is being performed.
+    """
+
+    #PTRACE_ATTACH or PTRACE_DETACH
+    op = ctypes.c_int(16 if attach else 17)
+    c_pid = c_pid_t(pid)
+    null = ctypes.c_void_p()
+
+    err = c_ptrace(op, c_pid, null, null)
+
+    if err != 0:
+        print err
 
 def dump(pid):
     """Dump the contents of memory to DB or another VNF."""
 
-    memory_permissions = 'rw'
-    dump = ''
+    memory_dump = ''
 
+    # sequentially read memory map
     with open('/proc/%d/maps' % pid, 'r') as maps_file:
         with open('/proc/%d/mem' % pid, 'r', 0) as mem_file:
             # for each mapped region
@@ -44,7 +66,6 @@ def dump(pid):
                 m = re.match(r'([0-9A-Fa-f]+)-([0-9A-Fa-f]+) ([-r][-w])', line)
 
                 if m.group(3) == memory_permissions:
-                    #sys.stderr.write("\nOK : \n" + line+"\n")
                     start = int(m.group(1), 16)
 
                     if start > 0xFFFFFFFFFFFF:
@@ -52,18 +73,45 @@ def dump(pid):
 
                     end = int(m.group(2), 16)
 
-                    #sys.stderr.write( "start = " + str(start) + "\n")
-
                     # seek to region start
                     mem_file.seek(start)
 
                     # read region contents
                     chunk = mem_file.read(end - start)
-                    dump += chunk
-                else:
-                    pass
-                    #sys.stderr.write("\nPASS : \n" + line+"\n")
-    return dump
+                    memory_dump += chunk
+
+    return memory_dump
+
+def restore(pid, dump_mem):
+    """Read memory map and replace with dump_mem contents."""
+
+    dump_mem_fd = open(dump_mem, 'r')
+
+    # sequentially read memory map
+    with open('/proc/%d/maps' % pid, 'r') as maps_file:
+        with open('/proc/%d/mem' % pid, 'w', 0) as mem_file:
+            # for each mapped region
+            for line in maps_file.readlines():
+                m = re.match(r'([0-9A-Fa-f]+)-([0-9A-Fa-f]+) ([-r][-w])', line)
+
+                if m.group(3) == memory_permissions:
+                    start = int(m.group(1), 16)
+
+                    if start > 0xFFFFFFFFFFFF:
+                        continue
+
+                    end = int(m.group(2), 16)
+
+                    # seek to region start
+                    mem_file.seek(start)
+
+                    # read dumped memory contents
+                    chunk = dump_mem_fd.read(end - start)
+
+                    # replace actual memory with readed content
+                    mem_file.write(chunk)
+
+    dump_mem_fd.close()
 
 def get_pid(container_id):
     """Given a VNF ID, search for the corresponding container and the returns the application PID."""
@@ -88,6 +136,22 @@ def get_pid(container_id):
     processes_pids = processes_pids.split('\n')[:-1]
     return int(processes_pids[1])
 
+def export_vnf_state(vnf_function_pid):
+    """ptrace and memory dump to export VNF state."""
+
+    ptrace(True, vnf_function_pid)    # stop process
+    state = dump(vnf_function_pid)    # dump memory
+    ptrace(False, vnf_function_pid)   # resume process
+
+    return state
+
+def import_vnf_state(vnf_function_pid, dump_mem):
+    """ptrace and replace process memory with dumped memory to import VNF state."""
+
+    ptrace(True, vnf_function_pid)      # stop process
+    restore(vnf_function_pid, dump_mem) # restore memory into VNF
+    ptrace(False, vnf_function_pid)     # resume process
+
 def sync_db(vnf_id, vnf_function_pid, cooldown):
     """Sync mechanism used by 0R (0 replicas) and 1R-AS (1 Replica Active-Standby).
     Periodically dump VNF memory and send directly to DB.
@@ -95,8 +159,8 @@ def sync_db(vnf_id, vnf_function_pid, cooldown):
 
     while True:
         try:
-            state = dump(vnf_function_pid)
-            write_state(vnf_id, state)
+            state = export_vnf_state(vnf_function_pid) # export VNF state
+            write_state(vnf_id, state) # save state to DB
         except Exception as e:
             print e
 
@@ -179,21 +243,11 @@ def sync_state():
     sync_vnf.daemon = True
     sync_vnf.start()
 
+    sync_workers[vnf_id] = sync_vnf.pid
+
+    print sync_workers
+
     return "ok"
-
-@app.route('/state/list', methods=['GET'])
-def list_states(self):
-    """List all saved states from all VNFs."""
-
-    pass
-
-@app.route('/state/get', methods=['GET'])
-def get_states(self, vnf_id):
-    """Get all saved states from a specific VNF."""
-
-    pass
-
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=9003)
